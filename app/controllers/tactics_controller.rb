236d9d42792dcb4a9866f2d78bec49be8b7ec47b
Linmiao Xu <linmiao.xu@gmail.com>
2016-03-05 18:23:41 -0800

    Show tactics set # on initial pageload

--- app/controllers/tactics_controller.rb
+++ app/controllers/tactics_controller.rb
@@ -1,7 +1,9 @@
 class TacticsController < ApplicationController
 
   def index
-
+    offset = params[:offset].to_i
+    @next_offset = offset + Puzzles::SET_SIZE
+    @tactics_set_id = (offset / Puzzles::SET_SIZE) + 1
   end
 
 end
