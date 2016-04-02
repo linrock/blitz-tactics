1a75a794b44001691de56ecf9abee09c268c46cd
Linmiao Xu <linmiao.xu@gmail.com>
2016-04-02 15:11:54 -0700

    Puzzles searchable by # pieces

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -14,7 +14,12 @@ class PuzzlesController < ApplicationController
   end
 
   def search
-    @puzzles = LichessPuzzle.rating_lt(1400).white_to_move.limit(100)
+    @puzzles = LichessPuzzle.rating_lt(1400).white_to_move
+    if params[:pieces].present?
+      @puzzles = @puzzles.n_pieces_eq(params[:pieces])
+    end
+    @n_puzzles = @puzzles.count
+    @puzzles = @puzzles.limit(100)
     respond_to do |format|
       format.html {}
       format.json {
