26444089fa5eff27dcba264a92c3676c7ba5d020
Linmiao Xu <linmiao.xu@gmail.com>
2016-04-02 21:40:45 -0700

    Support searching by different queries

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -15,9 +15,10 @@ class PuzzlesController < ApplicationController
 
   def search
     @puzzles = LichessPuzzle.rating_lt(1400).white_to_move
-    if params[:pieces].present?
-      @puzzles = @puzzles.n_pieces_eq(params[:pieces])
-    end
+    @puzzles = @puzzles.n_pieces_eq(params[:pieces]) if params[:pieces].present?
+    @puzzles = @puzzles.rating_lt(params[:max_rating]) if params[:max_rating].present?
+    @puzzles = @puzzles.attempts_gt(params[:min_attempts]) if params[:min_attempts].present?
+    @puzzles = @puzzles.vote_gt(params[:min_votes]) if params[:min_votes].present?
     @n_puzzles = @puzzles.count
     @puzzles = @puzzles.page(params[:page]).per(100)
     respond_to do |format|
