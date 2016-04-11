ed0f92c52ab8fa5e3892244512515628cffe13c0
Linmiao Xu <linmiao.xu@gmail.com>
2016-04-11 01:58:01 -0700

    Don't hardcode a max rating

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -14,9 +14,10 @@ class PuzzlesController < ApplicationController
   end
 
   def search
-    @puzzles = LichessPuzzle.rating_lt(1400).white_to_move
+    max_rating = params[:max_rating] || 1400
+    @puzzles = LichessPuzzle.white_to_move
     @puzzles = @puzzles.n_pieces_eq(params[:pieces]) if params[:pieces].present?
-    @puzzles = @puzzles.rating_lt(params[:max_rating]) if params[:max_rating].present?
+    @puzzles = @puzzles.rating_lt(max_rating) if params[:max_rating].present?
     @puzzles = @puzzles.attempts_gt(params[:min_attempts]) if params[:min_attempts].present?
     @puzzles = @puzzles.vote_gt(params[:min_votes]) if params[:min_votes].present?
     @n_puzzles = @puzzles.count
