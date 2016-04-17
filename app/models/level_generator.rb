3a0503de69e58dcd4b773ba94c85832ec40bb431
Linmiao Xu <linmiao.xu@gmail.com>
2016-04-17 14:48:54 -0700

    Level generator convenience methods

--- app/models/level_generator.rb
+++ app/models/level_generator.rb
@@ -4,12 +4,23 @@ class LevelGenerator
   N_LEVELS   = 50
 
   def puzzle_source
-    puzzles = LichessPuzzle.rating_lt(1400).vote_gt(28).white_to_move
-    puzzles.take(LEVEL_SIZE * N_LEVELS).shuffle(random: Random.new(1))
+    LichessPuzzle.rating_lt(1600).vote_gt(35).white_to_move
+  end
+
+  def deduplicated_source
+    existing = existing_puzzle_ids
+    puzzle_source.shuffle(:random => random_seed).select do |puzzle|
+      !existing.include?(puzzle.id)
+    end
+  end
+
+  def existing_puzzle_ids
+    Set.new(Level.all.map(&:puzzle_ids).flatten.uniq)
   end
 
   def build_levels!
-    puzzle_source.each_slice(LEVEL_SIZE).each_with_index do |level_puzzles, id|
+    puzzles = deduplicated_source.take(LEVEL_SIZE * N_LEVELS)
+    puzzles.each_slice(LEVEL_SIZE).each_with_index do |level_puzzles, id|
       puzzle_ids = level_puzzles.sort_by(&:rating).map(&:id)
       slug = level_slug(id + 1)
       Level.create!({ :slug => slug, :puzzle_ids => puzzle_ids })
@@ -28,6 +39,19 @@ class LevelGenerator
 
   def level_slug(id)
     "level-#{id}"
+  end
+
+  def add_new_level(puzzle_ids)
+    last_level = Level.last
+    new_level = Level.create!({ :slug => level_slug(last_level.id + 1),
+                                :puzzle_ids => puzzle_ids })
+    last_level.update_attribute :next_level_id, new_level.id
+  end
+
+  private
+
+  def random_seed
+    Random.new(1)
   end
 
 end
