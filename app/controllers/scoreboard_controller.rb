9b0c9c91546353b2baf72ca1cedcbdb916542c34
Linmiao Xu <linmiao.xu@gmail.com>
2016-06-09 00:29:00 -0700

    Link to scoreboard in main header

--- app/controllers/scoreboard_controller.rb
+++ app/controllers/scoreboard_controller.rb
@@ -3,7 +3,7 @@ class ScoreboardController < ApplicationController
   def index
     @ranked_users = User.all.sort_by do |user|
       -user.highest_level_unlocked
-    end.take(50)
+    end.take(30)
   end
 
 end
