57047190a4275029bc3b3ae148bf2df880441d44
Linmiao Xu <linmiao.xu@gmail.com>
2018-05-29 13:41:31 +0800

    Don't require unused railties and engines

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,15 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+require 'rails'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+require 'action_view/railtie'
+require 'action_mailer/railtie'
+# require 'active_job/railtie'
+# require 'action_cable/engine'
+# require 'active_storage/engine'
+# require 'rails/test_unit/railtie'
+require 'sprockets/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
