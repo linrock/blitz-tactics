258f9a59b110a0c8867602a981b94117e5e5d24f
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-20 10:30:08 +0800

    Add new lichess puzzle importer

---
+++ app/models/puzzle_importer.rb
@@ -0,0 +1,22 @@
+module PuzzleImporter
+
+  def import_from_json_files
+    existing_puzzle_ids = Set.new NewLichessPuzzle.pluck(:puzzle_id)
+    new_puzzle_files = Dir.glob("data/new-lichess/*.json").select do |filename|
+      !existing_puzzle_ids.include?(filename[/(\d+)/, 1].to_i)
+    end
+    puts "Found #{new_puzzle_files.length} puzzles to import"
+    new_puzzle_files.each do |filename|
+      puts filename
+      open(filename, 'r') do |f|
+        json_data = JSON.parse(f.read)
+        ActiveRecord::Base.logger.silence do
+          NewLichessPuzzle.find_or_create_from_raw_data(json_data)
+        end
+      end
+    end
+    true
+  end
+
+  extend self
+end
