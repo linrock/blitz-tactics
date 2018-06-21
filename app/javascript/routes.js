b373cf5771ad09145055035cdecf48b28984951a
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-21 13:21:35 +0800

    Remove unused position creator view

--- app/javascript/routes.js
+++ app/javascript/routes.js
@@ -2,16 +2,13 @@ import InfinityMode from './components/infinity_mode'
 import PositionTrainer from './components/position_trainer'
 import PrecisionMode from './components/precision_mode'
 import PuzzlePlayer from './components/puzzle_player'
-
 import LevelEditor from './views/level_editor'
-import PositionCreator from './views/position_creator'
 
 const routes = {
   "levels#show": PrecisionMode,
   "levels#edit": LevelEditor,
   "puzzles#show": PuzzlePlayer,
   "infinity#index": InfinityMode,
-  "static#positions": PositionCreator,
   "static#position": PositionTrainer,
   "positions#show": PositionTrainer,
 }
