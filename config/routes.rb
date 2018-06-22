79fc7f27b30ac001bd694621f8a99fa4b8e7732e
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-23 00:18:43 +0800

    Clean up routes a bit

--- config/routes.rb
+++ config/routes.rb
@@ -3,27 +3,6 @@ Rails.application.routes.draw do
 
   root 'home#index'
 
-  get '/puzzles/search'        => 'puzzles#search'
-  get '/puzzles/:id'           => 'puzzles#show'
-
-  get '/about'                 => 'static#about'
-  get '/positions'             => 'static#positions'
-  get '/position'              => 'static#position'
-
-  static_routes = StaticRoutes.new
-  static_routes.route_map!
-  static_routes.route_paths.each do |route|
-    get route => "static#defined_position"
-  end
-
-  get '/pawn-endgames'         => 'static#pawn_endgames'
-  # get '/rook-endgames'         => 'static#rook_endgames'
-
-  get '/levels'                => 'levels#index'
-  get '/scoreboard'            => 'scoreboard#index'
-
-  resources :positions, :only => [:show]
-
   # infinity mode
   get '/infinity'              => 'infinity#index'
   get '/infinity/puzzles'      => 'infinity#puzzles'
@@ -35,10 +14,33 @@ Rails.application.routes.draw do
 
   # repetition mode
   get '/level-:level_num'      => 'levels#show'
-  get '/level-:level_num/edit' => 'levels#edit'
-  put '/level-:level_num'      => 'levels#update'
   post '/levels/:id/attempt'   => 'levels#attempt'
   post '/levels/:id/complete'  => 'levels#complete'
+
+  # position trainer
+  get '/positions/:id'         => 'positions#show'
+  get '/positions'             => 'static#positions'
+  get '/position'              => 'static#position'
+
+  get '/about'                 => 'static#about'
+  get '/scoreboard'            => 'scoreboard#index'
+  get '/levels'                => 'levels#index'
+
+  # static routes
+  get '/pawn-endgames'         => 'static#pawn_endgames'
+  # get '/rook-endgames'         => 'static#rook_endgames'
+
+  static_routes = StaticRoutes.new
+  static_routes.route_map!
+  static_routes.route_paths.each do |route|
+    get route => "static#defined_position"
+  end
+
+  # admin routes
+  get '/level-:level_num/edit' => 'levels#edit'
+  put '/level-:level_num'      => 'levels#update'
+  get '/puzzles/search'        => 'puzzles#search'
+  get '/puzzles/:id'           => 'puzzles#show'
 
   # user routes
   get '/:username'             => 'users#show'
