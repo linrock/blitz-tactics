5e141ffb63eda9f5a27bc1531c1615d399e0d6a0
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-23 17:26:02 +0800

    Remove mobile meta viewport hack

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,28 +6,6 @@ window.config = {
   comboSizeForNextLevel: 100
 };
 
-// Set meta viewport
-//
-(() => {
-  const viewport = document.querySelector('meta[name="viewport"]'),
-        isIphone = navigator.userAgent.match(/iPhone/i),
-        setViewport = () => viewport.setAttribute('content', 'width=500, user-scalable=no')
-
-  if (!isIphone) {
-    return
-  }
-  setViewport()
-  window.addEventListener('resize', () => {
-    var w = window.innerWidth
-    var h = window.innerHeight
-    if (w > h) {
-      viewport.removeAttribute('content')
-    } else {
-      setViewport()
-    }
-  }, true)
-})()
-
 document.addEventListener('DOMContentLoaded', () => {
   const { controller, action } = document.querySelector('body').dataset
   const route = routes[`${controller}#${action}`]
@@ -35,9 +13,6 @@ document.addEventListener('DOMContentLoaded', () => {
   // initialize route components/views
   if (typeof route !== 'undefined') {
     new route
-  }
-  if (blitz.route) {
-    new routes[blitz.route]
   }
 
   // initialize all mini chessboards
