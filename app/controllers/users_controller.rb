3c560ed51e1c931d830cce4a30a700c2cfbadb98
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-23 14:43:53 +0800

    Render a 404 page instead of crashing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_username(params[:username])
+    unless @user.present?
+      render "pages/not_found", status: 404
+      return
+    end
     if @user == current_user
       render "users/me"
       return
