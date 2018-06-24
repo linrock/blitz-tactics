baa15a240d27d548e25ccc700082602b220852cc
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-24 17:35:08 +0800

    Don't require action mailer

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,7 @@ require 'rails'
 require 'active_record/railtie'
 require 'action_controller/railtie'
 require 'action_view/railtie'
-require 'action_mailer/railtie'
+# require 'action_mailer/railtie'
 # require 'active_job/railtie'
 # require 'action_cable/engine'
 # require 'active_storage/engine'
@@ -27,10 +27,6 @@ module BlitzTactics
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
 
     config.autoload_paths << Rails.root.join("app/view-models")
 
