1e069c701606c0626b1f14e057ebe5fcbf72f75a
Linmiao Xu <linmiao.xu@gmail.com>
2018-06-26 17:16:15 +0800

    Password confirmation and optional email during signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_sign_up_params, if: :devise_controller?
 
   protected
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+  def configure_permitted_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
   end
 
   def set_user
