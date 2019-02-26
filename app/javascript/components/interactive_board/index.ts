4238508456ab94187a79ef558a2bf4c17d037853
Linmiao Xu <linmiao.xu@gmail.com>
2019-02-26 12:27:54 +0800

    Fix tsc error

--- app/javascript/components/interactive_board/index.ts
+++ app/javascript/components/interactive_board/index.ts
@@ -4,8 +4,12 @@ import Chessboard from '../chessboard/chessboard'
 import MoveStatus from './views/move_status'
 import PiecePromotionModal from './views/piece_promotion_modal'
 
+interface BoardOptions {
+  noCombo?: boolean
+}
+
 export default class InteractiveBoard {
-  constructor(options = {}) {
+  constructor(options: BoardOptions = {}) {
     new Chessboard
     if (!options.noCombo) {
       new MoveStatus
