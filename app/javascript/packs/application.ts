5af98df9658a77982f6ccf5c88bdae55f7e2a46d
Linmiao Xu <linmiao.xu@gmail.com>
2020-10-02 14:04:03 -0400

    Go back to using sprockets for page sass files

--- app/javascript/packs/application.ts
+++ app/javascript/packs/application.ts
@@ -1,16 +1,3 @@
 // Webpack entry point. Imports JS and CSS to run on the page
 
-// TODO go back to using sprockets for component-independent CSS
-import '../stylesheets/main_header.sass'
-import '../stylesheets/responsive.sass'
-import '../stylesheets/pages/about.sass'
-import '../stylesheets/pages/homepage.sass'
-import '../stylesheets/pages/not_found.sass'
-import '../stylesheets/pages/positions_index.sass'
-import '../stylesheets/pages/puzzle_attempts.sass'
-import '../stylesheets/pages/puzzles_index.sass'
-import '../stylesheets/pages/registration.sass'
-import '../stylesheets/pages/scoreboard.sass'
-import '../stylesheets/pages/user_profile.sass'
-
 import '../index'
