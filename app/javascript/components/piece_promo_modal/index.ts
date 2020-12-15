156930c2a8bf950f766a8ca16001c8191fa8bbd7
Linmiao Xu <linmiao.xu@gmail.com>
2020-12-14 23:46:14 -0500

    Get piece promos working

--- app/javascript/components/piece_promo_modal/index.ts
+++ app/javascript/components/piece_promo_modal/index.ts
@@ -13,6 +13,27 @@ export default class PiecePromotionModal extends Backbone.View {
   private moveIntent: ChessMove
   private readonly cjs = new Chess
 
+  private template(color: 'w'): string {
+    const piecesHtml = ['q', 'r', 'n', 'b'].map((piece) => {
+      return `
+        <a class="piece" href="javascript:" data-piece="${piece}">
+          <svg viewBox="0 0 45 45">
+            <use xlink:href="#${color}${piece}" width="100%" height="100%"/>
+          </svg>
+        </a>
+      `
+    }).join('')
+    return `
+      <div class="piece-promotion-modal">
+        <div class="prompt">
+          Choose your destiny
+        </div>
+        <div class="pieces">${piecesHtml}</div>
+      </div>
+      <div class="background"></div>
+    `
+  }
+
   // @ts-ignore
   get el(): HTMLElement {
     return document.querySelector(`.piece-promotion-modal-container`)
@@ -35,12 +56,15 @@ export default class PiecePromotionModal extends Backbone.View {
   }
 
   private show() {
-    this.el.style.display = `block`
+    this.el.innerHTML = this.template('w')
+    this.el.style.display = 'block'
+    this.el.style.zIndex = '1000'
     Mousetrap.bind(`esc`, () => this.hide())
   }
 
   private hide() {
     this.el.style.display = `none`
+    this.el.style.zIndex = '0'
     Mousetrap.unbind(`esc`)
   }
 
