99fbad65a20f1feae2990b88bc712e6bbf5f94d7
Linmiao Xu <linmiao.xu@gmail.com>
2020-12-20 17:21:50 -0500

    Convert endgame positions pages to use slim template

--- app/javascript/components/puzzle_list/index.ts
+++ app/javascript/components/puzzle_list/index.ts
@@ -1,22 +1,23 @@
 import store from '@blitz/local_storage'
 
+// If the player made mistakes, marks puzzles in the list as having mistakes
 export default () => {
-  console.log('puzzles#index init!')
+  console.log('puzzles#index init')
   const data = store.get(window.location.pathname)
-  if (data) {
-    console.dir(data)
-    Object.entries(data).forEach(([puzzleId, mistakes]) => {
-      const puzzleEl = document.querySelector(`[data-puzzle-id="${puzzleId}"]`)
-      console.dir(mistakes)
-      if (puzzleEl) {
-        const puzzleMistakeInfoEl: HTMLElement = puzzleEl.querySelector('.puzzle-mistake-info')
-        if (puzzleMistakeInfoEl) {
-          const nMistakes = (mistakes as string[]).length
-          puzzleMistakeInfoEl.textContent = `${nMistakes} mistake${nMistakes > 1 ? 's' : ''}`
-        }
-      }
-    })
-  } else {
+  if (!data) {
     console.log('no puzzle info found in localStorage')
+    return
   }
+  console.dir(data)
+  Object.entries(data).forEach(([puzzleId, mistakes]) => {
+    const puzzleEl = document.querySelector(`[data-puzzle-id="${puzzleId}"]`)
+    console.dir(mistakes)
+    if (puzzleEl) {
+      const puzzleMistakeInfoEl: HTMLElement = puzzleEl.querySelector('.puzzle-mistake-info')
+      if (puzzleMistakeInfoEl) {
+        const nMistakes = (mistakes as string[]).length
+        puzzleMistakeInfoEl.textContent = `${nMistakes} mistake${nMistakes > 1 ? 's' : ''}`
+      }
+    }
+  })
 }
