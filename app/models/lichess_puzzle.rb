46ba78d9dace8a13a93859f5cfea5125c758c879
Linmiao Xu <linmiao.xu@gmail.com>
2020-12-29 12:42:01 -0500

    Clean up unused model

--- app/models/lichess_puzzle.rb
+++ app/models/lichess_puzzle.rb
@@ -1,2 +1,6 @@
 class LichessPuzzle < ActiveRecord::Base
+
+  def is_reportable?
+    false
+  end
 end
--- app/models/lichess_v2_puzzle.rb
+++
@@ -1,6 +0,0 @@
-class LichessV2Puzzle < ActiveRecord::Base
-
-  def is_reportable?
-    false
-  end
-end
