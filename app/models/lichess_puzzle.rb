241e066d472606e3207162c89fdd0314f948a703
Linmiao Xu <linmiao.xu@gmail.com>
2020-12-29 16:28:43 -0500

    DRY lines tree calculation in new lichess puzzles

--- app/models/lichess_puzzle.rb
+++ app/models/lichess_puzzle.rb
@@ -5,20 +5,25 @@ class LichessPuzzle < ActiveRecord::Base
     false
   end
 
+  # puzzle format used by blitz tactics game modes
+  def bt_puzzle_data
+    # TODO clean this up
+    {
+      id: puzzle_id,
+      fen: initial_fen,
+      lines: lines_tree,
+      initialMove: {
+        uci: moves_uci[0],
+      }
+    }
+  end
+
+  # data format used by puzzle pages
   def puzzle_data
-    lines_tree_root = {}
-    lines = lines_tree_root
-    last_move_uci = nil
-    moves_uci[1..].each do |move_uci|
-      lines[move_uci] = {}
-      lines = lines[move_uci]
-      last_move_uci = move_uci
-    end
-    lines[last_move_uci] = 'win'
     {
       initial_fen: initial_fen,
       initial_move_uci: moves_uci[0],
-      lines: lines_tree_root,
+      lines: lines_tree,
     }
   end
 
@@ -37,5 +42,21 @@ class LichessPuzzle < ActiveRecord::Base
       puzzle_data: puzzle_data,
       metadata: metadata,
     }
+  end
+
+  private
+
+  # converts an array of puzzle moves to old tree format
+  def lines_tree
+    lines_tree_root = {}
+    lines_tree = lines_tree_root
+    last_move_uci = nil
+    moves_uci[1..].each do |move_uci|
+      lines_tree[move_uci] = {}
+      lines_tree = lines_tree[move_uci]
+      last_move_uci = move_uci
+    end
+    lines_tree[last_move_uci] = 'win'
+    lines_tree_root
   end
 end
