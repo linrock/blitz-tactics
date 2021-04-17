d66262b10f6a040eafd1c786b80f14f784210857
Linmiao Xu <linmiao.xu@gmail.com>
2021-04-17 15:28:17 -0400

    Redirect to puzzle set page after creation

--- app/controllers/puzzle_sets_controller.rb
+++ app/controllers/puzzle_sets_controller.rb
@@ -5,6 +5,7 @@ class PuzzleSetsController < ApplicationController
   end
 
   def show
+    @puzzle_set = PuzzleSet.find_by(id: params[:id])
   end
 
   def new
@@ -19,13 +20,18 @@ class PuzzleSetsController < ApplicationController
       select {|puzzle_id| puzzle_id.length == 5 }
     lichess_v2_puzzles = LichessV2Puzzle.where(puzzle_id: filtered_puzzle_ids)
     if lichess_v2_puzzles.count > 0
-      puzzle_set = current_user.puzzle_sets.create!({
+      @puzzle_set = current_user.puzzle_sets.new({
         name: puzzle_set_params[:name],
         description: puzzle_set_params[:description]
       })
-      puzzle_set.lichess_v2_puzzles = lichess_v2_puzzles
+      ActiveRecord::Base.transaction do
+        @puzzle_set.save!
+        @puzzle_set.lichess_v2_puzzles = lichess_v2_puzzles
+      end
+      redirect_to "/ps/#{@puzzle_set.id}"
     else
       # No valid puzzle IDs found in the payload. Do nothing.
+      render status: 400
     end
   end
 
