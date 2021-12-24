29af7b94599494336dabe816c33fe4b68bb32281
Linmiao Xu <linmiao.xu@gmail.com>
2021-12-23 20:14:01 -0500

    Update some files from rails app:update

--- config/application.rb
+++ config/application.rb
@@ -1,15 +1,18 @@
-require File.expand_path('../boot', __FILE__)
+require_relative "boot"
 
-require 'rails'
-require 'active_record/railtie'
-require 'action_controller/railtie'
-require 'action_view/railtie'
-require 'action_mailer/railtie'
-# require 'active_job/railtie'
-# require 'action_cable/engine'
-# require 'active_storage/engine'
-# require 'rails/test_unit/railtie'
-require 'sprockets/railtie'
+require "rails"
+# Pick the frameworks you want:
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
+# require "active_storage/engine"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
+require "action_view/railtie"
+# require "action_cable/engine"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -18,15 +21,7 @@ Bundler.require(*Rails.groups)
 module BlitzTactics
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
-
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
+    config.load_defaults 7.0
 
     config.autoload_paths << Rails.root.join("app/models/creators")
     config.autoload_paths << Rails.root.join("app/models/importers")
@@ -38,5 +33,13 @@ module BlitzTactics
     config.assets.paths << Rails.root.join("app/javascript/game_modes")
 
     config.active_record.schema_format = :sql
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
   end
 end
