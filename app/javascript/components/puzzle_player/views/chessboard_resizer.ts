100301dfb89df1111b4236e172fcb3fd33caa538
Linmiao Xu <linmiao.xu@gmail.com>
2025-06-07 18:32:24 -0400

    Update svg resize icon

--- app/javascript/components/puzzle_player/views/chessboard_resizer.ts
+++ app/javascript/components/puzzle_player/views/chessboard_resizer.ts
@@ -84,7 +84,11 @@ export default class ChessboardResizer {
     // Create the drag handle element
     this.dragHandle = document.createElement('div')
     this.dragHandle.className = 'chessboard-drag-handle'
-    this.dragHandle.innerHTML = '⋰' // Resize icon
+    this.dragHandle.innerHTML = `
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 18">
+        <path fill="#494c4e" d="M14.228 16.227a1 1 0 0 1-.707-1.707l1-1a1 1 0 0 1 1.416 1.414l-1 1a1 1 0 0 1-.707.293zm-5.638 0a1 1 0 0 1-.707-1.707l6.638-6.638a1 1 0 0 1 1.416 1.414l-6.638 6.638a1 1 0 0 1-.707.293zm-5.84 0a1 1 0 0 1-.707-1.707L14.52 2.043a1 1 0 1 1 1.415 1.414L3.457 15.934a1 1 0 0 1-.707.293z"/>
+      </svg>
+    `
     this.dragHandle.title = 'Drag to resize board'
     
     // The chessboard already has absolute positioning from CSS, don't override it
