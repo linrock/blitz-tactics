1ec66d60428e6a343b3f8eb13a80ebdd0a2856de
Linmiao Xu <linmiao.xu@gmail.com>
2025-08-22 13:29:06 -0400

    Fix js watch compliation in development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,9 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # Enable asset compilation in development
+  config.assets.compile = true
+  config.assets.debug = true
+  config.assets.digest = false
 end
