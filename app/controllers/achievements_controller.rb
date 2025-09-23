d0b9dcd06ae8520bb92db34e0d421964c8573fc2
Linmiao Xu <linmiao.xu@gmail.com>
2025-09-23 14:19:13 -0400

    Use feature flag for achievements page

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,8 +1,17 @@
 class AchievementsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_achievements_feature_flag
 
   def index
     # This will be used to track user progress in the future
     # For now, we'll just display the static achievements list
+  end
+
+  private
+
+  def check_achievements_feature_flag
+    unless FeatureFlag.enabled?(:achievements)
+      raise ActionController::RoutingError, 'Not Found'
+    end
   end
 end
